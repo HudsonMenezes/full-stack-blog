5780190f8b3c0d81f14677006fb09e3cc8eac924
Hudson Menezes <hudson.bm23@gmail.com>
2023-02-03 14:06:26 -0300

    changes

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import { ArticlesList } from "../components/ArticlesList";
 import { NotFoundPage } from "./NotFoundPage";
+import axios from "axios";
 
 export const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+  }, []);
+
   const params = useParams();
   const articleId = params.articleId;
   const article = articles.find((article) => article.name === articleId);
@@ -15,6 +26,7 @@ export const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}

ba0574844e9584f43693614750d1c0eb2488497b
Hudson Menezes <hudson.bm23@gmail.com>
2023-02-09 15:17:07 -0300

    login and logout done, firebase auth complete

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "./hooks/useUser";
 
 export const NavBar = () => {
+  const { user } = useUser();
+  const navigate = useNavigate();
+
   return (
     <nav>
       <ul>
@@ -15,6 +20,25 @@ export const NavBar = () => {
           <Link to="/articles">Articles</Link>
         </li>
       </ul>
+      <div className="nav-right">
+        {user ? (
+          <button
+            onClick={() => {
+              signOut(getAuth());
+            }}
+          >
+            Log Out
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Log In
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
